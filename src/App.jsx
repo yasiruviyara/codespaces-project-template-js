97c93cf5b926edb72f0353fad8f403c9974cd849
Yasiru Viyara <80908044+yasiruviyara@users.noreply.github.com>
2024-11-20 15:04:50 +0000

    update 1

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Yasiru Viyara",
+  title: "Web Devloper",
+  email: "wemaster2003@gmail.com",
+  gitHub: "yasiruviyara",
+  instagram: "yasiruviyara",
+  linkedIn: "yasiruviyara",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#001787";
+const secondaryColor = "#ccd2ed";
 
 const App = () => {
   return (
